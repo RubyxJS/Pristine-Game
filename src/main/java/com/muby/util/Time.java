0b862108d1f32a4709b58752839c6a8d75a36bcc
Reuben <68828483+RubyxJS@users.noreply.github.com>
2022-08-31 13:43:33 +0100

    wip vertex and shaders

--- src/main/java/com/muby/util/Time.java
+++ src/main/java/com/muby/util/Time.java
@@ -1,7 +1,7 @@
 package com.muby.util;
 
 public class Time {
-    public static float timeStart = System.nanoTime();
+    public static double timeStart = System.nanoTime();
 
     public static float getTime() {
         return (float) ((System.nanoTime() - timeStart) * 1E-9);
